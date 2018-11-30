9cc5e637e642d7b322dfc179f8b7476a444bfc40
basilesimon <basile@basilesimon.fr>
2018-11-30 13:57:06 +0000

    Complete digital colour palette

--- packages/styles/src/index.js
+++ packages/styles/src/index.js
@@ -1,12 +1,42 @@
 export const colors = {
-  yellow: {
-    r: 224,
-    g: 171,
-    b: 38
-  },
-  blue: {
+  blueDigi: {
     r: 37,
     g: 66,
-    b: 81
-  }
+    b: 81,
+  },
+  yellowDigi: {
+    r: 224,
+    g: 171,
+    b: 38,
+  },
+  lightBlueDigi: {
+    r: 224,
+    g: 171,
+    b: 38,
+  },
+  yellowDigi: {
+    r: 128,
+    g: 177,
+    b: 226,
+  },
+  organgeDigi: {
+    r: 243,
+    g: 127,
+    b: 47,
+  },
+  tealDigi: {
+    r: 50,
+    g: 146,
+    b: 166,
+  },
+  purpleDigi: {
+    r: 108,
+    g: 60,
+    b: 94,
+  },
+  sandDigi: {
+    r: 218,
+    g: 207,
+    b: 193,
+  },
 };
--- packages/styles/stories/index.stories.js
+++ packages/styles/stories/index.stories.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import { storiesOf } from "@storybook/react";
+import { storiesOf } from '@storybook/react';
 
-import { colors } from "../src";
+import { colors } from '../src';
 
 const config = {
-  barColor: colors.yellow
+  barColor: colors.yellow,
 };
 
-storiesOf("Styles", module).add("colors", () => (
+storiesOf('Styles', module).add('Digital colours', () => (
   <ul>
     {Object.keys(colors).map((key, index) => {
       const { r, g, b } = colors[key];
@@ -17,9 +17,9 @@ storiesOf("Styles", module).add("colors", () => (
           <div
             key={index}
             style={{
-              width: "50px",
-              height: "50px",
-              backgroundColor: `rgb(${r}, ${g}, ${b})`
+              width: '50px',
+              height: '50px',
+              backgroundColor: `rgb(${r}, ${g}, ${b})`,
             }}
           />
           {key}

8bc0e74a785b1935fb20878acae0ebdbc2a8e803
basilesimon <basile@basilesimon.fr>
2018-11-30 14:59:23 +0000

    Simple story for line chart

--- packages/area/stories/index.stories.js
+++ packages/area/stories/index.stories.js
@@ -2,5 +2,9 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Area from '../src';
+import data from './fixtures/';
+const colours = ['#95D5A0', '#4D718B', '#80b1e2', '#f37f2f'];
 
-storiesOf('Charts/Area', module).add('default', () => <Area />);
+storiesOf('Charts/Area', module).add('default', () => (
+  <Area data={data} curve={'curveBasis'} percentage={true} yDomain={[0, 100]} />
+));

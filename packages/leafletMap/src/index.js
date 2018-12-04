a7a8b77641d01b24b2d1c6948ace42b65988b807
basilesimon <basile@basilesimon.fr>
2018-12-04 12:15:33 +0000

    Basic map centering on a point

--- packages/leafletMap/src/index.js
+++ packages/leafletMap/src/index.js
@@ -1,11 +1,61 @@
-import React from "react";
+/* global require, L */
+// Libraries
+import React from 'react';
+require('leaflet/dist/leaflet.css');
+import { Map, TileLayer, Marker, PopUp, GeoJSON } from 'react-leaflet';
+import L from 'leaflet';
 
-export default () => (
-  <main>
-    This is just a template, to help you create new components. Copy the{" "}
-    <code>./packages/template</code> directory to{" "}
-    <code>./packages/(your-package-name-here)</code> and update the{" "}
-    <code>package.json</code> and <code>README.md</code> with your package's
-    name and description.
-  </main>
-);
+// Styles
+import style from './style.scss';
+
+export class LeafletMap extends React.Component {
+  state = {
+    geoData: null,
+    position: [51.505513, -0.087792],
+  };
+  render() {
+    const { geoData, position } = this.state;
+    const icon = new L.Icon({
+      iconUrl:
+        '//components.timesdev.tools.s3.amazonaws.com/lib2/boundaries-map/assets/2273e3d8ad9264b7daa5bdbf8e6b47f8.png',
+      iconSize: [30, 42],
+      iconAnchor: [15, 42],
+      popupAnchor: [0, 0],
+      shadowUrl:
+        '//components.timesdev.tools.s3.amazonaws.com/lib2/boundaries-map/assets/44a526eed258222515aa21eaffd14a96.png',
+      shadowSize: [30, 30],
+      shadowAnchor: [15, 42],
+    });
+    return (
+      <div
+        id="container"
+        className={[style.container, !geoData && style.isLoading].join(' ')}
+      >
+        <Map
+          style={{ height: '500px' }}
+          zoomControl={true}
+          dragging={true}
+          center={position}
+          zoom={16}
+          ref="map"
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png"
+            attribution="Tiles courtesy of <a href=&quot;http://openstreetmap.se/&quot; target=&quot;_blank&quot;>OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href=&quot;http://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a>"
+          />
+          {geoData ? (
+            <GeoJSON
+              ref="boundaries"
+              data={geoData.features}
+              className={style.overlay}
+              onEachFeature={this.onEachFeature}
+            />
+          ) : null}
+          <Marker position={position} icon={icon} />
+        </Map>
+      </div>
+    );
+  }
+}
+
+export default LeafletMap;

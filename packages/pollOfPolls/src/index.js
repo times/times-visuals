3ff055a7105884de9cf3c16a683112f8c6bd64c2
Dan Clark <daniel.clark@the-times.co.uk>
2019-04-08 14:55:44 +0100

    Updated PollOfPolls

--- packages/pollOfPolls/src/index.js
+++ packages/pollOfPolls/src/index.js
@@ -1,13 +1,13 @@
 // Libraries
-import React from 'react';
+import React from "react";
 
-import * as selection from 'd3-selection';
-import * as jetpack from 'd3-jetpack';
-import * as scale from 'd3-scale';
-import * as timeFormat from 'd3-time-format';
-import * as array from 'd3-array';
-import * as shape from 'd3-shape';
-import * as axis from 'd3-axis';
+import * as selection from "d3-selection";
+import * as jetpack from "d3-jetpack";
+import * as scale from "d3-scale";
+import * as timeFormat from "d3-time-format";
+import * as array from "d3-array";
+import * as shape from "d3-shape";
+import * as axis from "d3-axis";
 
 const d3 = {
   ...selection,
@@ -16,17 +16,44 @@ const d3 = {
   ...timeFormat,
   ...array,
   ...shape,
-  ...axis,
+  ...axis
 };
 
 // Styles
-import style from './style.scss';
+import style from "./style.scss";
 
 export class PollOfPolls extends React.Component {
   state = { polls: null, averages: null, latestAverages: null };
 
   drawChart(node) {
-    node.innerHTML = '';
+    // check if start and end date properties are set and if so, filter the polling data and averages
+    const startDate = this.props.startDate
+      ? new Date(this.props.startDate)
+      : null;
+
+    const endDate = this.props.endDate ? new Date(this.props.endDate) : null;
+
+    const dataset = startDate
+      ? endDate
+        ? this.state.polls
+            .filter(poll => poll.date > startDate)
+            .filter(poll => poll.date < endDate)
+        : this.state.polls.filter(poll => poll.date >= startDate)
+      : endDate
+      ? this.state.polls.filter(poll => poll.date <= endDate)
+      : this.state.polls;
+
+    const averages = startDate
+      ? endDate
+        ? this.state.averages
+            .filter(average => average.date >= startDate)
+            .filter(average => average.date <= endDate)
+        : this.state.averages.filter(average => average.date >= startDate)
+      : endDate
+      ? this.state.averages.filter(average => average.date <= endDate)
+      : this.state.averages;
+
+    node.innerHTML = "";
     const { width } = node.getBoundingClientRect();
     const config = {
       padding: 0,
@@ -38,13 +65,15 @@ export class PollOfPolls extends React.Component {
         top: 50,
         right: 50,
         left: 50,
-        bottom: 50,
+        bottom: 50
       },
-      dataset: this.state.polls,
-      averages: this.state.averages,
+      dataset: dataset,
+      averages: averages,
       parties: this.props.parties,
       circleRadius: this.props.circleRadius ? this.props.circleRadius : 4,
       yDomainFromProps: this.props.yDomain ? this.props.yDomain : null,
+      startDate: startDate,
+      endDate: endDate,
       get yExtent() {
         return d3.extent(config.dataset.map(e => e.poll));
       },
@@ -58,7 +87,10 @@ export class PollOfPolls extends React.Component {
         return d3
           .scaleTime()
           .range([0, config.usableWidth])
-          .domain([new Date('2018-03-01'), new Date('2018-11-01')]);
+          .domain([
+            config.startDate ? config.startDate : new Date("2018-03-01"),
+            config.endDate ? config.endDate : new Date("2018-10-31")
+          ]);
       },
       get yScale() {
         return d3
@@ -67,168 +99,138 @@ export class PollOfPolls extends React.Component {
           .domain(
             config.yDomainFromProps ? config.yDomainFromProps : config.yExtent
           );
-      },
+      }
     };
 
     const svg = d3
       .select(node)
-      .append('svg')
+      .append("svg")
       .at({
         width: width,
-        height: config.height,
+        height: config.height
       });
 
     //Defining a group element for line chart
-    const g = svg.append('g');
-    g.translate([config.margin.left, config.margin.top]);
+    const g = svg.append("g").at({
+      transform: `translate(${config.margin.left},${config.margin.top})`
+    });
 
     // X-axis
     // renders full year on January,
     // short month names otherwise
-    g
-      .append('g')
-      .attr('class', 'axis axis--x')
+    g.append("g")
+      .at({
+        class: "axis axis--x"
+      })
       .translate([0, config.usableHeight])
       .call(
         d3
           .axisBottom(config.xScale)
           .ticks(config.ticksno)
+          .tickSizeOuter(0)
           .tickFormat(function(dataset) {
-            if (d3.timeFormat('%-b')(dataset) === 'Jan') {
-              return d3.timeFormat('%Y')(dataset);
+            if (d3.timeFormat("%-b")(dataset) === "Jan") {
+              return d3.timeFormat("%Y")(dataset);
             } else {
-              return d3.timeFormat('%-b')(dataset);
+              return d3.timeFormat("%-b")(dataset);
             }
           })
       );
 
     // Y-axis
     // with more or less padding and ticks depending on viewport
-    g
-      .append('g')
-      .attr('class', 'axis axis--y')
+    g.append("g")
+      .at({
+        class: "axis axis--y"
+      })
       .call(
         d3
           .axisLeft(config.yScale)
           .ticks(config.ticksno)
-          .tickSize(-width)
-          .tickPadding(20)
-          .tickFormat((d, i, n) => (n[i + 1] ? d : d + '%'))
+          .tickSizeInner(-width)
+          .tickSizeOuter(0)
+          .tickFormat((d, i, n) => (n[i + 1] ? d : d + "%"))
       );
 
-    g
-      .selectAll('circle')
+    g.selectAll("circle")
       .data(config.dataset)
       .enter()
-      .append('circle')
+      .append("circle")
       .at({
         cx: d => config.xScale(d.date),
         cy: d => config.yScale(d.poll),
-        r: config.circleRadius,
-        class: d => style[d.party],
+        r: config.circleRadius
       })
       .st({
         opacity: 0.2,
+        fill: d =>
+          config.parties.filter(
+            party => party.name.toLowerCase() === d.party
+          )[0]
+            ? config.parties.filter(
+                party => party.name.toLowerCase() === d.party
+              )[0].color
+            : ""
       });
 
     // One set of dots for each party
     // One line for each party
-    for (var i = 0; i < config.parties.length; i++) {
+    config.parties.forEach(party => {
       const popline = d3
         .line()
         .x(d => config.xScale(d.date))
-        .y(d => config.yScale(d[config.parties[i].name]))
+        .y(d => config.yScale(d[party.name]))
         .curve(d3.curveCardinal);
 
-      g
-        .append('path')
+      g.append("path")
         .datum(config.averages)
         .at({
-          fill: 'none',
-          stroke: '#fff',
+          fill: "none",
+          stroke: "#fff",
           strokeWidth: 3,
-          d: popline,
+          d: popline
         });
-      g
-        .append('path')
+      g.append("path")
         .datum(config.averages)
         .at({
-          fill: 'none',
-          stroke: config.parties[i].color,
+          fill: "none",
+          stroke: party.color,
           strokeWidth: 1.5,
-          d: popline,
+          d: popline
         });
-    }
-  }
-
-  extractLatestAverages(averages, parties) {
-    const lastAverages = averages[averages.length - 1];
-    let averagesArray = [];
-    for (var k in lastAverages) {
-      if (lastAverages.hasOwnProperty(k)) {
-        let temp = {};
-
-        // Extract party colour through lookup
-        const fill = parties.find(value => {
-          for (var i in Object.keys(lastAverages)) {
-            if (Object.keys(lastAverages)[i] === value.name) {
-              return value.color.toString();
-            }
-          }
-        });
-
-        // Build temporary array of results
-        // then push it Scotty
-        temp['party'] = k;
-        temp['partyshort'] = k.replace('/', '');
-        temp['poll'] = Math.round(parseFloat(lastAverages[k]));
-        temp['color'] = fill.color;
-        averagesArray.push(temp);
-      }
-    }
-    averagesArray.shift();
-    averagesArray = averagesArray.sort(function(a, b) {
-      return parseFloat(b.poll) - parseFloat(a.poll);
     });
-
-    this.setState({ latestAverages: averagesArray });
   }
 
-  // 2. Quick parsing of the data we pass in
+  // Quick parsing of the data we pass in
   // ie numbers as integers, dates as dates, etc.
   // Will trigger a re-render
-  makePollsAndAverages(polls, averages) {
-    const parseDate = d3.timeParse('%Y-%m-%d');
-    polls.forEach(function(d, i, o) {
-      if (d.poll != 'NA') {
+  makePollsAndAverages(polls, averages, parties) {
+    const parseDate = d3.timeParse("%Y-%m-%d");
+    polls.forEach((d, i, o) => {
+      if (d.poll != "NA") {
         d.date = new Date(d.date);
         d.poll = +d.poll;
       } else {
         o.splice(i, 1);
       }
     });
-    averages.forEach(function(d, i) {
+    averages.forEach((d, i) => {
       d.date = new Date(d.date);
-      d['AfD'] = +d['AfD'];
-      d['Green'] = +d['Green'];
-      d['CDU/CSU'] = +d['CDU/CSU'];
-      d['Left'] = +d['Left'];
-      d['FDP'] = +d['FDP'];
-      d['SPD'] = +d['SPD'];
+      parties.forEach(party => {
+        d[party.name] = +d[party.name];
+      });
     });
 
     this.setState({ polls, averages });
   }
 
-  // 1. Pick out most recent average polling
-  // Sort descending
-  // We use this to build the key at the bottom
-  // 2. Quick parsing of the data we pass in
-  // ie numbers as integers, dates as dates, etc.
   componentDidMount() {
     if (this.chart && this.props.data && this.props.averages) {
-      this.makePollsAndAverages(this.props.data, this.props.averages);
-      this.extractLatestAverages(this.props.averages, this.props.parties);
+      this.makePollsAndAverages(
+        this.props.data,
+        this.props.averages,
+        this.props.parties
+      );
     }
   }
 
@@ -240,25 +242,29 @@ export class PollOfPolls extends React.Component {
   }
 
   render() {
-    const { latestAverages } = this.state;
+    const { parties, dataSource } = this.props;
     return (
       <div className={style.Container}>
         <div ref={node => (this.chart = node)} />
-        {latestAverages ? (
+        {parties ? (
           <div className={style.labelsContainer}>
-            {latestAverages.map((e, key) => (
-              <div className={[style.label, style.text].join(' ')} key={key}>
+            {parties.map((e, key) => (
+              <div className={[style.label, style.text].join(" ")} key={key}>
+                <div className={style.circle} style={{ background: e.color }} />
                 <div
-                  className={[style.number, style[e.partyshort]].join(' ')}
+                  className={[style.number, style[e.name]].join(" ")}
                   key={key}
                 >
-                  {e.poll}
+                  {e.latest}%
                 </div>
-                {e.party}
+                {e.long_name ? e.long_name : e.name}
               </div>
             ))}
           </div>
         ) : null}
+        <p className={style.source}>
+          Source: {dataSource ? dataSource : "PollOfPolls.eu"}
+        </p>
       </div>
     );
   }
--- packages/pollOfPolls/stories/index.stories.js
+++ packages/pollOfPolls/stories/index.stories.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from "react";
+import { storiesOf } from "@storybook/react";
 
-import { parties, polls, averages } from './fixtures';
-import PollOfPolls from '../src';
+import { parties, polls, averages } from "./fixtures";
+import PollOfPolls from "../src";
 
-storiesOf('Projects/PollOfPolls', module)
-  .add('default', () => (
+storiesOf("Projects/PollOfPolls", module)
+  .add("default", () => (
     <PollOfPolls parties={parties} data={polls} averages={averages} />
   ))
-  .add('Ridiculous circles', () => (
+  .add("Ridiculous circles", () => (
     <PollOfPolls
       parties={parties}
       data={polls}
@@ -16,11 +16,28 @@ storiesOf('Projects/PollOfPolls', module)
       circleRadius={10}
     />
   ))
-  .add('Custom y domain', () => (
+  .add("Custom y domain", () => (
     <PollOfPolls
       parties={parties}
       data={polls}
       averages={averages}
       yDomain={[0, 100]}
+    />
+  ))
+  .add("Custom date range", () => (
+    <PollOfPolls
+      parties={parties}
+      data={polls}
+      averages={averages}
+      startDate={"2018-06-01"}
+      endDate={"2018-09-10"}
+    />
+  ))
+  .add("Custom data source", () => (
+    <PollOfPolls
+      parties={parties}
+      data={polls}
+      averages={averages}
+      dataSource={"Times research"}
     />
   ));

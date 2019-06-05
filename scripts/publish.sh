4007b725ef44adfde90f8a9b476a2891474f9c1a
Chris Hutchinson <chris_hutchinson@me.com>
2019-06-05 13:51:17 +0100

    chore: update publish script to include docs

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Storybook docs
+if ! git diff-index --quiet HEAD --
+then
+  git add .
+  git commit -m "chore: add docs"
+fi;
+
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
 
 MESSAGE=$(printf "chore(release): publish %s [skip ci]")
 
 yarn lerna publish --conventional-commits --yes --concurrency=1 --exact -m "$MESSAGE"
+
+git push

8e30f74e28f3ec79e678a2ec7078254790d8fb5f
Chris Hutchinson <chris_hutchinson@me.com>
2019-06-05 12:42:34 +0100

    chore: Add flag to publish command

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,4 +4,4 @@ echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
 
 MESSAGE=$(printf "chore(release): publish %s [skip ci]")
 
-yarn lerna publish --conventional-commits --yes --concurrency=1 --exact -m "$MESSAGE"
+yarn lerna publish --no-git-reset --conventional-commits --yes --concurrency=1 --exact -m "$MESSAGE"

dc055b74087ea30f2ba934f64df7029a15ada609
Chris Hutchinson <chris_hutchinson@me.com>
2019-06-13 14:41:22 +0100

    Add README to stories

--- packages/postcode-search/stories/index.stories.js
+++ packages/postcode-search/stories/index.stories.js
@@ -2,9 +2,16 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
+import Readme from "../README.md";
+
 import PostcodeSearch from "../src";
 
 storiesOf("Elements/Postcode Search", module)
+  .addParameters({
+    readme: {
+      sidebar: Readme
+    }
+  })
   .add("default", () => (
     <PostcodeSearch
       onLoad={action("Postcode loaded")}

0696a2b8d55280a78625656d609db66fdd918e6d
Dan Clark <daniel.clark@the-times.co.uk>
2019-09-05 18:02:31 +0100

    fix codeship parser error

--- packages/component-wrapper/src/index.js
+++ packages/component-wrapper/src/index.js
@@ -5,7 +5,7 @@ import { WrapperContainer } from "./style";
 export class Wrapper extends React.Component {
   render() {
     const { children } = this.props;
-    return <WrapperContainer>{children && { children }}</WrapperContainer>;
+    return <WrapperContainer>{children && children}</WrapperContainer>;
   }
 }
 
